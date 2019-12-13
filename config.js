27719d19f2b38b1c1e80bc8d4b35505b5e4c0f82
marczeller <pierarte@gmail.com>
2019-12-13 15:43:20 +0000

    Added SAI2DAI.js script

--- config.js
+++ config.js
@@ -8,10 +8,13 @@ exports.config = {
     provider : 'https://mainnet.infura.io/v3/a3456c11324745b7966ca0208ca8de4b',
     // this is the minimum amount of DAI needed before launching the script, don't put it too low! (Gas attacks)
     threshold : '20',
-    // where you want the chai to go for storage (I recommend an Hardware wallet)
-    GoToAddress : '0x010afb8548a5D1a3a3D62f58CA0a5A1329974206',
+    // where you want the chai to go for storage (I recommend an Hardware wallet) you can input an ENS address here.
+    GoToAddress : 'marczeller.eth',
     // tokens smart-contracts address
     DAIAddress : '0x6B175474E89094C44Da98b954EedeAC495271d0F',
-    chaiAddress : '0x06AF07097C9Eeb7fD685c692751D5C66dB49c215'
+    chaiAddress : '0x06AF07097C9Eeb7fD685c692751D5C66dB49c215',
+    SAIAddress : '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
+    migrationContractAddress : '0xc73e0383f3aff3215e6f04b0331d58cecf0ab849',
+
 
 }
